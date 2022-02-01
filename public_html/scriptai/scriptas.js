6bffc858eca7ce673219a09023fa7cf435e786b8
TomasJurkenas <tomisarius18@gmail.com>
2022-02-01 20:37:23 +0100

    sachmatuLenta.html

---
+++ public_html/scriptai/scriptas.js
@@ -0,0 +1,12 @@
+let a = 1;
+let kin ="kinatmojo a reiksme :"
+console.log(kin+a)
+
+let b="Tomas Jurkenas"
+let c="mano vardas"
+console.log(c+b)
+
+let metai=1994;
+let menuo=3;
+let diena=20;
+console.log(metai%10+menuo%10+diena%10)
