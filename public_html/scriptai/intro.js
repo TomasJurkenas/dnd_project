4c462d4cf61769b6a84dc1c58fcff341df0140cb
TomasJurkenas <tomisarius18@gmail.com>
2022-02-13 22:52:18 +0100

    projektas

---
+++ public_html/scriptai/intro.js
@@ -0,0 +1,23 @@
+let button = document.getElementById('registrationForm');
+
+let openForm = function (){
+
+    button.style.display="flex";
+}
+let closeForm = function (){
+
+    button.style.display="none"
+
+}
+$('#regBtn').click(function(){
+    $('#regBtn').animate({
+        left :'250px',
+        opacity: '0,5',
+        height :'150px',
+        width : '150px'
+    });
+    return false;
+});
+
+
+
