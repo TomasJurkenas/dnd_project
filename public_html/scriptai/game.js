45a2f953e6d95b30a51c2ff481f3d706dee359c2
TomasJurkenas <tomisarius18@gmail.com>
2022-02-27 21:11:48 +0100

    projektas

--- public_html/scriptai/game.js
+++ public_html/scriptai/game.js
@@ -1,12 +1,79 @@
-// $("#chooseHero").typer({
-//     encouragement:["Choose your hero",
-//         "Choose your warrior",
-//         "Pick up your weapon",
-//         "can you just choose?",
-//         "Okay this is annoying just choose!"],
-//
-//
-// });
+!(function (a) {
+    "use strict";
+    a.fn.typer = function (b) {
+        function c(a, b) {
+            k < b.length
+                ? ((g = b[k].split("")),
+                    (h = g.length),
+                    setTimeout(function () {
+                        a.append(g[j]),
+                            j++,
+                            j < h
+                                ? c(a, b)
+                                : ((j = 0),
+                                    k++,
+                                    setTimeout(function () {
+                                        e(a, function () {
+                                            c(a, b);
+                                        });
+                                    }, i.backspaceDelay));
+                    }, i.typeSpeed))
+                : i.repeat && d(a, b);
+        }
+        function d(a, b) {
+            (k = 0),
+                setTimeout(function () {
+                    c(a, b);
+                }, i.repeatDelay);
+        }
+        function e(a, b) {
+            setTimeout(function () {
+                a.text(a.text().slice(0, -1)), 0 < a.text().length ? e(a, b) : "function" == typeof b && b();
+            }, i.backspaceSpeed);
+        }
+        function f(a) {
+            setInterval(function () {
+                a.fadeOut(400).fadeIn(400);
+            }, 900);
+        }
+        let g,
+            h,
+            i = a.extend({ typeSpeed: 60, backspaceSpeed: 20, backspaceDelay: 800, repeatDelay: 1e3,
+                repeat: !0, autoStart: !0, startDelay: 100, useCursor: !0, strings: [""] }, b),
+            j = 0,
+            k = 0;
+        return this.each(function () {
+            let b,
+                d,
+                e = a(this);
+            i.autoStart &&
+            (e.append('<span class="typed"></span>'),
+            i.useCursor && (e.append('<span class="typed_cursor">&#x7c;</span>'), (d = e.children(".typed_cursor")), f(d)),
+                (b = e.children(".typed")),
+                setTimeout(function () {
+                    c(b, i.strings);
+                }, i.startDelay));
+        });
+    };
+})(jQuery);
+
+
+
+$("#text").typer({
+    strings:["Choose your hero",
+        "Choose your warrior",
+        "Pick up your weapon",
+        "Can you just choose?",
+        "Okay this is annoying just choose!"],
+    typeSpeed: 200,
+    backspaceSpeed: 20,
+    backspaceDelay: 800,
+    repeatDelay: 1000,
+    repeat: true,
+    autoStart: true,
+    startDelay: 100,
+
+});
 
 
 let slideIndex = 1;
